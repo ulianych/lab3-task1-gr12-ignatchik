3fd7137dd6e30c68d1cd73eb4db5b3b8d169f9d6
ulianych <86627982+ulianych@users.noreply.github.com>
2024-02-27 22:20:37 +0000

    Test function findNumbersWithMaxDigitSum

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include "sum.h"
+#include <time.h>
 
 void main (void)
 {
-	printf( "Sum of digits of number %d is %d\n", 123, sumOfDigits(123));
+  int size = 3;
+  int *arr = (int *)malloc(size * sizeof(int));
+  
+  srand(time(NULL));
+  for (int i = 0; i < size; i++) {
+      arr[i] = rand() % 1000;
+  }
+
+  for (int i = 0; i < 3; i++) {
+      printf("%d ", arr[i]);
+  }
+  findNumbersWithMaxDigitSum(arr, size);
+
+  free(arr);
 }
